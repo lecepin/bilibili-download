68656493dc244355bbf5d55cfa98400408eb02cf
lecepin <383810086@qq.com>
2022-05-01 22:48:08 +0800

    clear

--- src/electron/ipc.js
+++ src/electron/ipc.js
@@ -9,46 +9,6 @@ const path = require('path');
 let lastChildProcess;
 
 const initIPC = () => {
-  ipcMain.on('merge-merge', (event, arg) => {
-    if (lastChildProcess) {
-      lastChildProcess.kill();
-      lastChildProcess = null;
-    }
-
-    let cmd = 'ffmpeg';
-    let env = {
-      ...process.env,
-      PATH: '/usr/local/bin:' + child_process.execSync('echo $PATH').toString(),
-    };
-
-    if (os.platform() == 'win32') {
-      cmd = APP_CONFIG_TOOL_PATH;
-    }
-
-    lastChildProcess = spawn(cmd, ['-y', '-i', `concat:${arg.input}`, '-c', 'copy', arg.output], {
-      env: env,
-    });
-
-    lastChildProcess.stdout.on('data', data => {
-      event.reply('merge-merge-result', {
-        type: 'stdout',
-        data: data.toString(),
-      });
-    });
-    lastChildProcess.stderr.on('data', data => {
-      event.reply('merge-merge-result', {
-        type: 'stderr',
-        data: data.toString(),
-      });
-    });
-    lastChildProcess.on('error', data => {
-      event.reply('merge-merge-result', {
-        type: 'err',
-        data: data.toString(),
-      });
-    });
-  });
-
   ipcMain.on('get-video-info', (event, arg) => {
     getDonwloadUrl(arg)
       .then(data => {
